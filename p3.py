8e7d74fe55b4f2e1dc3a788d559c3e237eb2c0e9
Grisha_Pinigin <pin.pinigin@mail.ru>
2022-11-20 15:20:10 +0500

    +

--- p3.py
+++ p3.py
@@ -2,3 +2,19 @@
 # Каждому месяцу соответствует своя строка. 
 # Определите самый жаркий и самый холодный 7-дневный промежуток этого периода. 
 # Выведите его даты.
+from random import randint as rdt
+
+may = [rdt(12, 30) for _ in range(0, 31)]
+june = [rdt(13, 36) for _ in range(0, 30)]
+july = [rdt(16, 37) for _ in range(0, 31)]
+august = [rdt(13, 33) for _ in range(0, 31)]
+september = [rdt(4, 25) for _ in range(0, 30)]
+
+m = [may, june, july, august, september]
+
+for row in m:
+    print(row)
+
+for i in m:
+    for j in i:
+        
